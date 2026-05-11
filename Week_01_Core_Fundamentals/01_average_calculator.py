a54f1971cf25a6e79eec99e01ee316a26415c636
BirdBra1n <46430@rws.uk.net>
2026-05-11 20:55:37 +0100

    Update 01_average_calculator.py

--- Week_01_Core_Fundamentals/01_average_calculator.py
+++ Week_01_Core_Fundamentals/01_average_calculator.py
@@ -14,10 +14,23 @@ TODO:
 - Add demonstration code under `if __name__ == "__main__":`
 """
 
+def calculate_average(values):
+    sumNums = 0
+    count = 0
+    if values == [""]:
+        return 0
+    else:
+        for num in values:
+            sumNums += int(num.strip())
+            count += 1
+        return sumNums / count
+
 def main():
-    pass
-
-
+values = []
+numList = input("Enter a list of numbers: ")
+values += numList.split(",")
+print(numList)
+print(calculate_average(values))
 
 if __name__ == "__main__":
     main()
