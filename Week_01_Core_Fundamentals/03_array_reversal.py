130fd84654bf718012e1c7d0cd3cf4c4d9c5f1c9
BirdBra1n <46430@rws.uk.net>
2026-05-13 21:24:14 +0100

    Update 03_array_reversal.py

--- Week_01_Core_Fundamentals/03_array_reversal.py
+++ Week_01_Core_Fundamentals/03_array_reversal.py
@@ -12,12 +12,19 @@ TODO:
 - Add demonstration code under `if __name__ == "__main__":`
 """
 
-def main():
-    # TODO: Write demonstration/testing code
-    # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
-    # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
-    pass
+def reverse_list_inplace(values):
+    n = len(values)
+    for i in range(n // 2):
+        temp = values[i]
+        values[i] = values[n - 1 - i]
+        values[n - 1 - i] = temp
+    return values
 
+def main():
+    random_integers = [random.randint(1, 100) for _ in range(8)]
+    print(random_integers)
+    reversed_data = reverse_list_inplace(random_integers)
+    print(reversed_data)
 
 if __name__ == "__main__":
     main()
