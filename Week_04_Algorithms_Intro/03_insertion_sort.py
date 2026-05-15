75ef58952c22616f25c362190fa02816eb07833b
BirdBra1n <46430@rws.uk.net>
2026-05-15 11:51:17 +0100

    Update 03_insertion_sort.py

--- Week_04_Algorithms_Intro/03_insertion_sort.py
+++ Week_04_Algorithms_Intro/03_insertion_sort.py
@@ -13,19 +13,22 @@ TODO:
 """
 
 def insertion_sort(inputList): 
+    count = 0
     for element_index in range(1, len(inputList)): 
         currentValue = inputList[element_index] 
         position = element_index - 1 
         while position >= 0 and currentValue < inputList[position]: 
+            count += 1
             inputList[position + 1] = inputList[position] 
             position -= 1 
         inputList[position + 1] = currentValue 
-    return inputList
+    return inputList, count
 
 def main():
     myList = [1, 5, 7, 9, 4, 2, 3, 6, 10, 8] 
     sortedList = insertion_sort(myList) 
     print(sortedList) 
+    print(count)
 
 if __name__ == "__main__":
     main()
