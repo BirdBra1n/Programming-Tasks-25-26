b5f58b1ff6b1179454d1ed3fb305531a6c2a88c3
BirdBra1n <46430@rws.uk.net>
2026-09-22 19:35:01 +0100

    Update 01_CSV_Writer.py

--- Week_03_Arrays_Lists_CSV_Basics/01_CSV_Writer.py
+++ Week_03_Arrays_Lists_CSV_Basics/01_CSV_Writer.py
@@ -14,11 +14,80 @@ TODO:
 - Add demonstration code under `if __name__ == "__main__":`
 """
 
+import csv
+
+fileName = "user_data.csv"
+
+
+def appendRecord():
+    userName = input("Enter Name: ")
+    userAge = input("Enter Age: ")
+    favouriteColour = input("Enter Favourite Colour: ")
+    extraNote = input("Enter Anything Else: ")
+    csvFile = open(fileName, mode="a", newline="")
+    csvWriter = csv.writer(csvFile)
+    
+    csvWriter.writerow([userName, userAge, favouriteColour, extraNote])
+    csvFile.close()
+    
+    print("Record added successfully.")
+
+
+def readCsv():
+    csvFile = open(fileName, mode="r")
+    csvReader = csv.reader(csvFile)
+    
+    fileData = list(csvReader)
+    csvFile.close()
+
+    print("\n--- CSV File Contents ---")
+    for rowNumber in range(len(fileData)):
+        print(rowNumber, fileData[rowNumber])
+        
+    return fileData
+
+
+def editRecord():
+    fileData = readCsv()
+    
+    rowToEdit = int(input("\nEnter the row number you want to edit: "))
+    
+    print(f"Editing row {rowToEdit}: {fileData[rowToEdit]}")
+    newUserName = input("Enter New Name: ")
+    newUserAge = input("Enter New Age: ")
+    newFavouriteColour = input("Enter New Favourite Colour: ")
+    newExtraNote = input("Enter New Anything Else: ")
+
+    fileData[rowToEdit] = [newUserName, newUserAge, newFavouriteColour, newExtraNote]
+
+    csvFile = open(fileName, mode="w", newline="")
+    csvWriter = csv.writer(csvFile)
+    csvWriter.writerows(fileData)
+    csvFile.close()
+    
+    print("Record updated successfully.")
+
+
 def main():
-    # TODO: Write demonstration/testing code
-    # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
-    # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
-    pass
+    userChoice = ""
+    
+    while userChoice != "4":
+        print("\n--- Menu ---")
+        print("1. Read File")
+        print("2. Append Record")
+        print("3. Edit Record")
+        print("4. Exit")
+        
+        userChoice = input("Choose an option (1-4): ")
+
+        if userChoice == "1":
+            readCsv()
+        elif userChoice == "2":
+            appendRecord()
+        elif userChoice == "3":
+            editRecord()
+        elif userChoice == "4":
+            print("Goodbye!")
 
 
 if __name__ == "__main__":
